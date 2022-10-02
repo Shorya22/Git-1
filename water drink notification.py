16ca697a5ed4cd1b4e257b6a3155565f7db2c83d
“[Shorya <[shoryamathura22@gmail.com]>
2022-10-02 12:33:54 +0530

    Master

--- water drink notification.py
+++ water drink notification.py
@@ -8,5 +8,5 @@ if __name__== "__main__":
     title = "Please Drink Water",
     message = "Drinking water is like washing out your insides. The water will cleanse the system, fill you up, decrease your caloric load and improve the function of all your tissues.",
 
-    timeout = 10
+    timeout = 50
     )
