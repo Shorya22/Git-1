0182434b8e5506216218fde090b724eb749aee62
“[Shorya <[shoryamathura22@gmail.com]>
2022-10-02 10:27:21 +0530

    1st Git file

---
+++ water drink notification.py
@@ -0,0 +1,12 @@
+from msilib.schema import Icon
+from multiprocessing.spawn import _main
+import time
+from plyer import notification
+
+if __name__== "__main__":
+    notification.notify (
+    title = "Please Drink Water",
+    message = "Drinking water is like washing out your insides. The water will cleanse the system, fill you up, decrease your caloric load and improve the function of all your tissues.",
+
+    timeout = 30
+    )
